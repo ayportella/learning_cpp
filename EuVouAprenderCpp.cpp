617d58049decd3c034eb4302a53fb9ba53a89117
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-07-19 15:01:31 -0300

    Create EuVouAprenderCpp.cpp

---
+++ EuVouAprenderCpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+///Andressa Yumi Portella
+/* Este programa coloca na tela
+as palavras EU VOU APRENDER C++
+com quebra de linha entre as palavras*/
+
+int main()
+
+{ 
+    
+    std::cout << "EU" << std::endl << "VOU" << std::endl << "APRENDER" << std::endl << "C++" << std::endl;
+    
+    system("color 0A");
+    system("Pause");
+    
+    return 0;
+    
+
+} 
