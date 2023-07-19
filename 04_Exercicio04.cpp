45ea9df8be8d47872e5f05efc5f06f20c3f38800
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-07-19 15:24:54 -0300

    Create 04_Exercicio04.cpp

---
+++ 04_Exercicio04.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+///Andressa Yumi Portella
+//Data 17/07/2023
+/* Este programa a seguir coloca na tela a frase: Vou aprender a programar na Linguagem C++. Contudo cada palavra será exibida em uma única linha através da inserção de quebra de linhas*/
+
+int main()
+
+{ 
+    system("Color 47");
+    std::cout 
+        << "Vou" << "\n"
+        << "Aprender" << "\n"
+        << "a" << "\n"
+        << "programar" << "\n"
+        << "na" << "\n"
+        << "Linguage C++" << "\n";
+    
+    system("Pause");
+    
+    return 0;
+}
