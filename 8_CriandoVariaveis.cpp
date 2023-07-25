a524cbec6bf01cd1b2311f2bb305d8df0e01691b
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-07-25 10:16:19 -0300

    Create 8_CriandoVariaveis.cpp

---
+++ 8_CriandoVariaveis.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int NumComputadores;
+    int SerialComputadores;
+    NumComputadores = 100;
+    SerialComputadores = 11111;
+    std::cout << "$$$$$$$$$$$$$$" << std::endl;
+    std::cout << "Numero de Computadores: " << NumComputadores << std::endl;
+    std::cout << "Serial de Computadores: " << SerialComputadores << std::endl;
+    std::cout << "$$$$$$$$$$$$$$" << std::endl;
+
+    system("PAUSE");
+
+}
