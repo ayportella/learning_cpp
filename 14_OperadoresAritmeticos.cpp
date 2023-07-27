d993014b6f554674c6a65b9de6c501be0c69ef4d
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-07-27 10:49:03 -0300

    Update 14_OperadoresAritmeticos.cpp

--- 14_OperadoresAritmeticos.cpp
+++ 14_OperadoresAritmeticos.cpp
@@ -4,11 +4,11 @@ int main()
 {
     int Numero01, Numero02;
     int Resultado;
-    std::cout << "Digite o primeiro numero: ";
+    std::cout << "Digite o primeiro numero: " << "\n";
     std::cin >> Numero01;
-    std::cout << "Digite o segundo numero: ";
+    std::cout << "Digite o segundo numero: " << "\n";
     std::cin >> Numero02;
-    std::cout << "Numero02 = " << Numero02 << "\n";
+    std::cout << Numero02 << "\n";
     Resultado = Numero01 % Numero02;
     std::cout << "O Resto da divisão de dois numeros: " << Resultado << "\n";
     system("PAUSE");
