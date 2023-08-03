fb87816a9ac3648a5cbfa001a0f434b3253f80ba
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-08-03 14:54:30 -0300

    Create 25_Num0a50Pares.cpp

---
+++ 25_Num0a50Pares.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int Numero = 0;
+    while << (Numero <=50)
+    {
+        if (Numero % 2 == 0)
+        {
+            std::cout << Numero << " ";
+        }
+    //De alguma forma voce precisa incrementar a variavel numero para que ela 
+    //chegue ate 50
+    //Qual condicao de parada de loop?
+    Numero++;
+    }
+    system("PAUSE");
+    return 0;
+
+}
